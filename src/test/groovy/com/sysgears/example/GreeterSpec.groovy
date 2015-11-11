e049530a3fce797b331e5bce401e9ca217486211
Steve Coffman <gears@umich.edu>
2015-11-11 14:28:58 -0500

    -Fix tests by excluding ServletTestExecutionListener, DirtiesContextTestExecutionListener, TransactionalTestExecutionListener, SqlScriptsTestExecutionListener from TestExecutionListeners\ -Add jacoco and cobertura test coverage, nicer test console output, html reports

--- src/test/groovy/com/sysgears/example/GreeterSpec.groovy
+++ src/test/groovy/com/sysgears/example/GreeterSpec.groovy
@@ -1,16 +1,21 @@
 package com.sysgears.example
 
 import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.TestExecutionListeners
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener
 import spock.lang.Specification
 
 import javax.inject.Inject
 
+@TestExecutionListeners(inheritListeners = false, listeners = [DependencyInjectionTestExecutionListener.class])
 @ContextConfiguration(classes = AppConfig.class)
 class GreeterSpec extends Specification {
-    private @Inject Greeter greeter
-    
+	private @Inject
+	Greeter greeter
+
     def 'check greeting'() {
         expect: 'should generate correct greet string'
-        greeter.greet('Hello', 'Test') == 'Hello, Test! (from greeter 1)' 
+        greeter.greet('Hello', 'Test') == 'Hello, Test! (from greeter 1)'
     }
 }
